88b328e8de602309f743ab052f6bb5ff88ef2f16
Kevin Harbal <kevin.harbal@fr.ibm.com>
2015-08-10 13:48:56 +0200

    test Mac OS

--- tests/intern.js
+++ tests/intern.js
@@ -20,6 +20,8 @@ define({
 		{ browserName: "firefox", version: "31", platform: "Windows 7", name : "delite" },
 		{ browserName: "chrome", version: "36", platform: "Windows 7", name : "delite" },
 		{ browserName: "safari", version: "7", platform: "OS X 10.9", name : "delite" },
+        { browserName: "firefox", version: "31", platform: "OS X 10.9", name : "delite" },
+        { browserName: "chrome", version: "36", platform: "OS X 10.9", name : "delite" },
 
 		// Mobile
 		{ platformName: "iOS", platformVersion: "7.1", browserName: "safari", deviceName: "iPhone Simulator",
